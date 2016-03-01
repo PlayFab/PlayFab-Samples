688504246d01b39f9608f2b6daddc48dd7973672
zac <zac@playfab.com>
2016-03-01 12:48:58 -0800

    added comments to cloud script files

--- Recipes/RegeneratingCurrency/CloudScript.js
+++ Recipes/RegeneratingCurrency/CloudScript.js
@@ -1,11 +1,12 @@
 // defining these up top so we can easily change these later if we need to.
-var CHANCE_TO_DIE = 0.3333;
-var GEM_MAX = 20;
-var GEM_MIN = 10;                                 // this could also be set in TitleData
-var LIVES_CURRENCY_CODE = "LV";
-var GEMS_CURRENCY_CODE = "GM";
+var CHANCE_TO_DIE = 0.3333;						  // % chance to lose a life during battle, this could also be set in TitleData
+var GEM_MAX = 20;								  // maximum limit on the gold that can be found, this could also be set in TitleData
+var GEM_MIN = 10;                                 // maximum limit on the gold that can be found, this could also be set in TitleData
+var LIVES_CURRENCY_CODE = "LV";					  // currecny code for our Lives VC
+var GEMS_CURRENCY_CODE = "GM";					  // currency code for our Gems VC
 
 handlers.Battle = function(args) {
+	// get the calling player's inventory and VC balances
 	var GetUserInventoryRequest = {
         "PlayFabId": currentPlayerId
     };
@@ -15,12 +16,13 @@ handlers.Battle = function(args) {
 	var userVcBalances = GetUserInventoryResult.VirtualCurrency;
 	var userVcRecharge = GetUserInventoryResult.VirtualCurrencyRechargeTimes;
 
+	// make sure the player has > 0 lives before proceeding. 
 	if(!CheckLives(userVcBalances))
 	{
 		throw "No lives remaining. Purchase additional lives or wait: " + userVcRecharge[LIVES_CURRENCY_CODE].SecondsToRecharge + " seconds.";
 	}
 
-	// calculate the battle...
+	// calculate the battle using our 'global' params...
 	var gemsFound = Math.floor(Math.random() * (GEM_MAX - GEM_MIN + 1) + GEM_MIN);
 	AddVc(userVcBalances, GEMS_CURRENCY_CODE, gemsFound);
 	log.info("You found " + gemsFound + " gems.");
