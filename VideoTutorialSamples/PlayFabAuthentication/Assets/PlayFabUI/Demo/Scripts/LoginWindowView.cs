1607f241bada012084d04837ba5a73e68d6529cd
Marco Williams <marco@playfab.com>
2017-10-09 17:00:36 -0700

    AuthService is singleton now
    Added ability to Uncheck RememberMe after logged in.

--- VideoTutorialSamples/PlayFabAuthentication/Assets/PlayFabUI/Demo/Scripts/LoginWindowView.cs
+++ VideoTutorialSamples/PlayFabAuthentication/Assets/PlayFabUI/Demo/Scripts/LoginWindowView.cs
@@ -7,7 +7,10 @@ using PlayFab;
 using PlayFab.ClientModels;
 
 public class LoginWindowView : MonoBehaviour {
+    //Debug Flag to simulate a reset
     public bool ClearPlayerPrefs;
+
+    //Meta fields for objects in the UI
     public InputField Username;
     public InputField Password;
     public InputField ConfirmPassword;
@@ -22,20 +25,31 @@ public class LoginWindowView : MonoBehaviour {
 
     public PlayFab.UI.ProgressBarView ProgressBar;
 
+    //Meta references to panels we need to show / hide
     public GameObject RegisterPanel;
     public GameObject Panel;
     public GameObject Next;
 
+    //Settings for what data to get from playfab on login.
     public GetPlayerCombinedInfoRequestParams InfoRequestParams;
 
+    //Reference to our Authentication service
+    private PlayFabAuthService _AuthService = PlayFabAuthService.Instance;
 
-    public string _PlayFabId;
-    public string _SessionTicket;
-
-    private PlayFabAuthService _AuthService = new PlayFabAuthService();
 
     public void Awake()
     {
+        if (ClearPlayerPrefs)
+        {
+            _AuthService.UnlinkSilentAuth();
+            _AuthService.ClearRememberMe();
+            _AuthService.AuthType = Authtypes.None;
+        }
+
+        //Set our remember me button to our remembered state.
+        RememberMe.isOn = _AuthService.RememberMe;
+
+        //Subscribe to our Remember Me toggle
         RememberMe.onValueChanged.AddListener((toggle) =>
         {
             _AuthService.RememberMe = toggle;
@@ -44,21 +58,19 @@ public class LoginWindowView : MonoBehaviour {
 
     public void Start()
     {
-        if (ClearPlayerPrefs)
-        {
-            _AuthService.AuthType = Authtypes.None;
-        }
 
+        //Hide all our panels until we know what UI to display
         Panel.SetActive(false);
         Next.SetActive(false);
         RegisterPanel.SetActive(false);
 
+        //Subscribe to events that happen after we authenticate
         PlayFabAuthService.OnDisplayAuthentication += OnDisplayAuthentication;
         PlayFabAuthService.OnLoginSuccess += OnLoginSuccess;
         PlayFabAuthService.OnPlayFabError += OnPlayFaberror;
 
 
-        //Setup Button Bindings
+        //Bind to UI buttons to perform actions when user interacts with the UI.
         LoginButton.onClick.AddListener(OnLoginClicked);
         PlayAsGuestButton.onClick.AddListener(OnPlayAsGuestClicked);
         LoginWithFacebook.onClick.AddListener(OnLoginWithFacebookClicked);
@@ -66,24 +78,35 @@ public class LoginWindowView : MonoBehaviour {
         RegisterButton.onClick.AddListener(OnRegisterButtonClicked);
         CancelRegisterButton.onClick.AddListener(OnCancelRegisterButtonClicked);
 
+        //Set the data we want at login from what we chose in our meta data.
         _AuthService.InfoRequestParams = InfoRequestParams;
 
+        //Start the authentication process.
         _AuthService.Authenticate();
     }
 
+
+    /// <summary>
+    /// Login Successfully - Goes to next screen.
+    /// </summary>
+    /// <param name="result"></param>
     private void OnLoginSuccess(LoginResult result)
     {
         Debug.LogFormat("Logged In as: {0}", result.PlayFabId);
-        _PlayFabId = result.PlayFabId;
-        _SessionTicket = result.SessionTicket;
-
+        
+        //Show our next screen if we logged in successfully.
         Panel.SetActive(false);
         Next.SetActive(true);
     }
 
+    /// <summary>
+    /// Error handling for when Login returns errors.
+    /// </summary>
+    /// <param name="error"></param>
     private void OnPlayFaberror(PlayFabError error)
     {
-
+        //There are more cases which can be caught, below are some
+        //of the basic ones.
         switch (error.Error)
         {
             case PlayFabErrorCode.InvalidEmailAddress:
@@ -100,16 +123,35 @@ public class LoginWindowView : MonoBehaviour {
                 break;
                 
         }
+
+        //Also report to debug console, this is optional.
         Debug.Log(error.Error);
         Debug.LogError(error.GenerateErrorReport());
     }
 
-
+    /// <summary>
+    /// Choose to display the Auth UI or any other action.
+    /// </summary>
     private void OnDisplayAuthentication()
     {
+        //Here we have choses what to do when AuthType is None.
         Panel.SetActive(true);
+
+        /*
+         * Optionally we could Not do the above and force login silently
+         * 
+         * _AuthService.Authenticate(Authtypes.Silent);
+         * 
+         * This example, would auto log them in by device ID and they would
+         * never see any UI for Authentication.
+         * 
+         */
     }
 
+    /// <summary>
+    /// Play As a guest, which means they are going to silently authenticate
+    /// by device ID or Custom ID
+    /// </summary>
     private void OnPlayAsGuestClicked()
     {
 
@@ -120,18 +162,34 @@ public class LoginWindowView : MonoBehaviour {
             ProgressBar.UpdateProgress(0f);
         });
 
-        _AuthService.AuthType = Authtypes.Silent;
-        _AuthService.Authenticate();
+        _AuthService.Authenticate(Authtypes.Silent);
     }
 
+    /// <summary>
+    /// Login Button means they've selected to submit a username (email) / password combo
+    /// Note: in this flow if no account is found, it will ask them to register.
+    /// </summary>
     private void OnLoginClicked()
     {
-        _AuthService.AuthType = Authtypes.EmailAndPassword;
+        ProgressBar.UpdateLabel(string.Format("Logging In As {0} ...", Username.text));
+        ProgressBar.UpdateProgress(0f);
+        ProgressBar.AnimateProgress(0, 1, () => {
+            //second loop
+            ProgressBar.UpdateProgress(0f);
+            ProgressBar.AnimateProgress(0, 1, () => {
+                ProgressBar.UpdateLabel(string.Empty);
+                ProgressBar.UpdateProgress(0f);
+            });
+        });
+
         _AuthService.Email = Username.text;
         _AuthService.Password = Password.text;
-        _AuthService.Authenticate();
+        _AuthService.Authenticate(Authtypes.EmailAndPassword);
     }
 
+    /// <summary>
+    /// No account was found, and they have selected to register a username (email) / password combo.
+    /// </summary>
     private void OnRegisterButtonClicked()
     {
         if (Password.text != ConfirmPassword.text)
@@ -140,24 +198,50 @@ public class LoginWindowView : MonoBehaviour {
             return;
         }
 
-        _AuthService.AuthType = Authtypes.RegisterPlayFabAccount;
+        ProgressBar.UpdateLabel(string.Format("Registering User {0} ...", Username.text));
+        ProgressBar.UpdateProgress(0f);
+        ProgressBar.AnimateProgress(0, 1, () => {
+            //second loop
+            ProgressBar.UpdateProgress(0f);
+            ProgressBar.AnimateProgress(0, 1, () => {
+                ProgressBar.UpdateLabel(string.Empty);
+                ProgressBar.UpdateProgress(0f);
+            });
+        });
+
         _AuthService.Email = Username.text;
         _AuthService.Password = Password.text;
-        _AuthService.Authenticate();
+        _AuthService.Authenticate(Authtypes.RegisterPlayFabAccount);
     }
 
+    /// <summary>
+    /// They have opted to cancel the Registration process.
+    /// Possibly they typed the email address incorrectly.
+    /// </summary>
     private void OnCancelRegisterButtonClicked()
     {
+        //Reset all forms
+        Username.text = string.Empty;
+        Password.text = string.Empty;
+        ConfirmPassword.text = string.Empty;
+        //Show panels
         RegisterPanel.SetActive(false);
         Next.SetActive(false);
     }
 
 
+    /// <summary>
+    /// Login with a facebook account.  This kicks off the request to facebook
+    /// </summary>
     private void OnLoginWithFacebookClicked()
     {
 
     }
 
+
+    /// <summary>
+    /// Login with a google account.  This kicks off the request to google play games.
+    /// </summary>
     private void OnLoginWithGoogleClicked()
     {
 
